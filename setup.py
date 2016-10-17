48d29d7cc56c097ebdfbc281ee9f68873abe4835
Hamza Sheikh <hamza.sheikh@ijettechnologies.com>
2016-10-17 11:13:21 -0700

    Fork python-sasl to work with qpid-tools

--- setup.py
+++ setup.py
@@ -7,19 +7,19 @@ setup.py file for SWIG example
 from setuptools import setup, Extension
 
 sasl_module = Extension('_saslwrapper',
-                           sources=['sasl/saslwrapper.cpp', "sasl/saslwrapper_wrap.cxx"],
-                           include_dirs=["sasl"],
+                           sources=['saslfork/saslwrapper.cpp', "saslfork/saslwrapper_wrap.cxx"],
+                           include_dirs=["saslfork"],
                            libraries=["sasl2"],
                            language="c++",
                            )
-dist = setup (name = 'sasl',
-       version = '0.1.3',
-       url = "http://github.com/toddlipcon/python-sasl/tree/master",
-       maintainer = "Todd Lipcon",
-       maintainer_email = "todd@cloudera.com",
-       description = """Cyrus-SASL bindings for Python""",
+dist = setup (name = 'saslfork',
+       version = '0.0.1',
+       url = "https://github.com/ijettech-dev/python-sasl/tree/master",
+       maintainer = "iJet Technologies",
+       maintainer_email = "qa@ijettechnologies.com",
+       description = """Cyrus-SASL bindings for Python forked from https://github.com/toddlipcon/python-sasl""",
        ext_modules = [sasl_module],
-       py_modules = ["sasl.saslwrapper"],
+       py_modules = ["saslfork.saslwrapper"],
        include_package_data = True,
        )
 
